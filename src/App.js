b13bfc8577bf6b0f852cdec8c5359f7d0b27d2a2
sumin <suminc255@gmail.com>
2024-04-18 10:15:52 +0900

    🚧 Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./components/section/Main";
+import Login from "./components/contents/Login";
 
 const Home = lazy(() => import("./pages/Home"));
 const PageTest = lazy(() => import("./pages/PageTest"));
+const MyPage = lazy(() => import("./pages/MyPage"));
+const Saved = lazy(() => import("./pages/Saved"));
+const MenPage = lazy(() => import("./pages/MenPage"));
+const WomenPage = lazy(() => import("./pages/WomenPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
 
 function App() {
   return (
@@ -12,6 +18,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pagestest" element={<PageTest />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/men" element={<MenPage />} />
+          <Route path="/women" element={<WomenPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "../components/section/Main";
+import Login from "../components/contents/Login";
+
+export default function Pages3() {
+  return (
+    <Main>
+      <Login />
+    </Main>
+  );
+}
