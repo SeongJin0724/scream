0c97283c6368c31b68a02fd5d5b2f4a06173bb66
dohun <jiho3332@naver.com>
2024-04-19 15:00:11 +0900

    🚧

--- src/components/data/footerdata.js
+++ src/components/data/footerdata.js
@@ -11,6 +11,7 @@ import Talented from "../../pages/Talented";
 import Partnership from "../../pages/Partnership";
 import Terms from "../../pages/Terms";
 import Personal from "../../pages/Personal";
+import { Link } from "react-router-dom";
 
 export const uldata1 = [
   {
@@ -62,47 +63,70 @@ export const uldata3 = [
   },
 
   {
-    content: <p className="p9"> 1:1 문의하기는 앱에서만 가능합니다.</p>,
+    content: <p className="p8"> 1:1 문의하기는 앱에서만 가능합니다.</p>,
   },
 ];
-// export const uldata2 = [
-//   {
-//     title: "notice",
-//     src: <Notice />,
-//   },
-//   {
-//     title: "service",
-//     src: <Service />,
-//   },
-//   {
-//     title: "storeinfo",
-//     src: <Storeinfo />,
-//   },
-//   {
-//     title: "Acceptance",
-//     src: <Acceptance />,
-//   },
-// ];
+export const uldata4 = [
+  {
+    title: "회사소개",
+    src: "/company",
+  },
+  {
+    title: "인재채용",
+    src: "/talented",
+  },
+  {
+    title: "제휴제안",
+    src: "/partnership",
+  },
+  {
+    title: "이용약관",
+    src: "/terms",
+  },
+];
+export const uldata5 = [
+  {
+    content: (
+      <p className="p1">
+        크림 주식회사 · 대표 김창욱<span className="blank"></span>사업자등록번호
+        : 570-88-01618
+        <Link className="p10" id="p10" to={"/question"}>
+          사업자정보확인
+        </Link>
+        <span className="blank"></span>
+        통신판매업 : 제 2021-성남분당C-0093호 사업장소재지 : 경기도 성남시
+        분당구 분당내곡로 131 판교테크원 타워1, 8층호스팅 서비스 : 네이버
+        클라우드 ㈜
+      </p>
+    ),
+  },
+];
 
-// export const uldata3 = [
-//   {
-//     title: "company",
-//     src: <Company />,
-//   },
-//   {
-//     title: "talented",
-//     src: <Talented />,
-//   },
-//   {
-//     title: "partnership",
-//     src: <Partnership />,
-//   },
-//   {
-//     title: "terms",
-//     src: <Terms />,
-//   },
-//   {
-//     title: "personal",
-//     src: <Personal />,
-//   },
-// ];
+export const uldata6 = [
+  {
+    content: <p className="p2">신한은행 채무지급보증 안내</p>,
+  },
+
+  {
+    content: (
+      <p className="p3">
+        당사는 고객님의 현금 결제 금액에 대해 신한은행과 채무지급보증 계약을
+        체결하여 안전거래를 보장하고 있습니다.
+        <Link className="p11" id="p11" to={"/question"}>
+          서비스가입 사실 확인
+        </Link>
+      </p>
+    ),
+  },
+  {
+    content: (
+      <p className="p4">
+        크림(주)는 통신판매 중개자로서 통신판매의 당사자가 아닙니다. 본 상품은
+        개별판매자가 등록한 상품으로 상품, 상품정보, 거래에 관한 의무와 책임은
+        각 판매자에게 있습니다. 단, 이용약관 및 정책, 기타 거래 체결 과정에서
+        고지하는 내용 등에 따라 검수하고 보증하는 내용에 대한 책임은 크림(주)에
+        있습니다.
+      </p>
+    ),
+  },
+];
--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 
-import { uldata1, uldata2, uldata3 } from "../data/footerdata";
+import {
+  uldata1,
+  uldata2,
+  uldata3,
+  uldata4,
+  uldata5,
+  uldata6,
+} from "../data/footerdata";
 import ModalContent from "../contents/ModalContent";
 import { FcCloseUpMode } from "react-icons/fc";
 import { FaCamera } from "react-icons/fa";
@@ -21,6 +28,7 @@ export default function Footer() {
         <div className="footer_top">
           <nav className="footer_nav">
             <ul className="footer_info">
+              <strong className="menu_title">이용안내</strong>
               {uldata1.map((item, index) => (
                 <li key={index} onClick={() => openModal(item.src)}>
                   {item.title}
@@ -33,6 +41,7 @@ export default function Footer() {
           </nav>
           <nav className="footer_nav2">
             <ul className="footer_info2">
+              <strong className="menu_title2">고객지원</strong>
               {uldata2.map((item, index) => (
                 <li key={index}>
                   <Link to={item.src}>{item.title}</Link>
@@ -58,52 +67,41 @@ export default function Footer() {
         </div>
         <div className="footer_bottom">
           <nav className="footer_nav3">
-            <ul className="footer_info4">
-              <li>
-                <Link to={"/company"}>회사소개</Link>
-              </li>
-              <li>
-                <Link to={"/talented"}>인재채용</Link>
-              </li>
-              <li>
-                <Link to={"/partnership"}>제휴제안</Link>
-              </li>
-              <li>
-                <Link to={"/terms"}>이용약관</Link>
-              </li>
-              <li>
-                <Link to={"/personal"} className="info41">
-                  개인정보처리방침
+            <div className="footer_info4_wrap">
+              <ul className="footer_info4">
+                {uldata4.map((item, index) => (
+                  <li key={index}>
+                    <Link to={item.src}>{item.title}</Link>
+                  </li>
+                ))}
+
+                <strong className="menu_title3">개인정보처리방침</strong>
+              </ul>
+              <div className="footer_info4_icon">
+                <Link to={"/company"}>
+                  <FcCloseUpMode className="FcCloseUpMode" />
                 </Link>
-              </li>
-            </ul>
+                <Link to={"/company"}>
+                  <FaCamera className="FaCamera" />
+                </Link>
+                <Link to={"/company"}>
+                  <FcCloseUpMode className="FcCloseUpMode" />
+                </Link>
+              </div>
+            </div>
+
+            {uldata5.map((item, index) => (
+              <p className="p_wrap" key={index}>
+                {item.content}
+              </p>
+            ))}
           </nav>
-          <div className="footer_info5">
-            <p className="p1">
-              주식회사 · 대표 ㅇㅇㅇ 사업자등록번호 : 000-11-22222 통신판매업 :
-              제 0123-C-4567호
-            </p>
-            <p className="p2">
-              사업장소재지 : 경기도 성남시 호스팅 서비스 : 네이버 클라우드 ㈜
-            </p>
-            <p className="p3">신한은행 채무지급보증 안내</p>
-            <p className="p4">
-              당사는 고객님의 현금 결제 금액에 대해 신한은행과 채무지급보증
-              계약을 체결하여 안전거래를 보장하고 있습니다.
-            </p>
-            <p className="p5">
-              ㅇㅇ은 통신판매 중개자로서 통신판매의 당사자가 아닙니다. 본 상품은
-              개별판매자가 등록한 상품으로 상품, 상품정보, 거래에 관한 의무와
-              책임은 각 판매자에게 있습니다. 단, 이용약관 및 정책, 기타 거래
-              체결 과정에서 고지하는 내용 등에 따라 검수하고 보증하는 내용에
-              대한 책임은 ㅇㅇ에 있습니다.
-            </p>
-            <Link to={"/company"}>
-              <FcCloseUpMode className="FcCloseUpMode" />
-            </Link>
-            <Link to={"/company"}>
-              <FaCamera className="FaCamera" />
-            </Link>
+          <div className="footer_nav4">
+            {uldata6.map((item, index) => (
+              <p className="p_wrap2" key={index}>
+                {item.content}
+              </p>
+            ))}
           </div>
         </div>
       </div>
