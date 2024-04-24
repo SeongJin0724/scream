cb655f34e803c6c8c99b408635cdb998eef6a2b5
jimmy <skawk3235@gmail.com>
2024-04-24 15:36:01 +0900

    🚧 search

--- src/pages/SearchRes.jsx
+++ src/pages/SearchRes.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate, Link } from "react-router-dom";
 import Main from "../components/section/Main";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 const SearchRes = () => {
   const [results, setResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -35,34 +38,65 @@ const SearchRes = () => {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   };
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim()) {
+      navigate(`/searchres?term=${encodeURIComponent(searchTerm)}`);
+    }
+  };
+
   return (
-    <Main>
-      {results.length > 0 ? (
-        <div className="result_wrap">
-          <h3 className="result_title">
-            검색결과: {capitalizeFirstLetter(searchTerm)}
-          </h3>
-          <div className="result_inner">
-            {results.map((product) => (
-              <div className="result_card" key={product.itemKey}>
-                <img src={product.img} alt={product.title} />
-                <p className="brandName">
-                  {capitalizeFirstLetter(product.brand)}
-                </p>
-                <h3 className="productName">{product.title}</h3>
-                <p className="price">
-                  {new Intl.NumberFormat("ko-KR").format(product.launchPrice)}원
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-      ) : (
-        <div className="result_wrap">
-          <p style={{ margin: "20px" }}>검색하신 결과가 없습니다.</p>
-        </div>
-      )}
-    </Main>
+    <>
+      <form onSubmit={handleSearch} className="searchres_search">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="브랜드,상품,프로필,태그 등"
+        />
+        {searchTerm && (
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            onClick={() => setSearchTerm("")}
+            className="clear_btn"
+          />
+        )}
+      </form>
+
+      <Main>
+        <ul className="search_result_lists">
+          {results.length > 0 ? (
+            results.map((product) => (
+              <li key={product.itemKey} className="search_result_wrap">
+                <Link>
+                  <img
+                    src={product.img}
+                    alt={product.title}
+                    className="search_item_img"
+                  />
+                  <div className="search_item_desc">
+                    <p className="brandName">
+                      {capitalizeFirstLetter(product.brand)}
+                    </p>
+                    <p className="productName">{product.title}</p>
+                    <p className="price">
+                      {new Intl.NumberFormat("ko-KR").format(
+                        product.launchPrice
+                      )}
+                      원
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            ))
+          ) : (
+            <div>
+              <p>검색하신 결과가 없습니다.</p>
+            </div>
+          )}
+        </ul>
+      </Main>
+    </>
   );
 };
 
