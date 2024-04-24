71fe9fdb7ad004d0753e567868a43598edf54c13
jimmy <skawk3235@gmail.com>
2024-04-24 18:22:23 +0900

    🚧 landing_brand

---
+++ src/pages/BrandProducts.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import Main from "../components/section/Main";
+import axios from "axios";
+
+const BrandProductsPage = () => {
+  const { brand } = useParams();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/brands/${brand}`
+        );
+        setProducts(response.data[0]);
+      } catch (error) {
+        console.error("제품을 로드하는 중 오류가 발생했습니다.", error);
+      }
+    };
+    fetchProducts();
+  }, [brand]);
+
+  return (
+    <Main>
+      <div>
+        <h2>{brand} 제품</h2>
+        <ul>
+          {products.length > 0 ? (
+            products.map((product) => (
+              <li key={product.itemKey}>
+                <Link>
+                  <img src={product.img} alt={product.title} />
+                  <div>
+                    <h3>{product.title}</h3>
+                    <p>{product.launchPrice}원</p>
+                  </div>
+                </Link>
+              </li>
+            ))
+          ) : (
+            <p>해당 브랜드의 제품이 없습니다.</p>
+          )}
+        </ul>
+      </div>
+    </Main>
+  );
+};
+
+export default BrandProductsPage;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import Carousel from "../components/contents/Carousel";
 import Main from "./../components/section/Main";
+import Carousel from "../components/contents/Carousel";
+import TopBrand from "../components/contents/TopBrand";
+
 export default function Home() {
   return (
     <Main>
       <Carousel />
-      <div className="home_wrap"></div>
+      <div className="home_wrap">
+        <TopBrand />
+      </div>
     </Main>
   );
 }
