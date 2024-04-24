4b834d44978c8a18db497e4956e88b89ee8fa38d
lsj <thls702464@gmail.com>
2024-04-24 16:40:23 +0900

    🚧 메인 캐러셀작업,로고수정,commonCSS수정

--- src/components/contents/Carousel.jsx
+++ src/components/contents/Carousel.jsx
@@ -4,12 +4,10 @@ import "swiper/css";
 import "swiper/css/autoplay";
 
 import { Autoplay } from "swiper/modules";
+import { mainSlideImg } from "../data/mainSlideImg";
 
-// import slide1 from "../../assets/img/slide_img1.webp";
-// import slide2 from "../../assets/img/slide_img2.webp";
-// import slide3 from "../../assets/img/slide_img3.webp";
-// import slide4 from "../../assets/img/slide_img4.webp";
-// import slide5 from "../../assets/img/slide_img5.webp";
+import slide1 from "../../assets/image/mainSlideImg1.webp";
+import slide2 from "../../assets/image/mainSlideImg2.webp";
 
 export default function Carousel() {
   return (
@@ -21,25 +19,10 @@ export default function Carousel() {
       loop={true}
       modules={[Autoplay]}
     >
-      <SwiperSlide>
-        {/* <img src={slide1} alt="slide1" /> */}
-        img1
-      </SwiperSlide>
-      <SwiperSlide>
-        {/* <img src={slide2} alt="slide2" /> */}
-        img2
-      </SwiperSlide>
-      <SwiperSlide>
-        {/* <img src={slide3} alt="slide3" /> */}
-        img3
-      </SwiperSlide>
-      <SwiperSlide>
-        {/* <img src={slide4} alt="slide4" /> */}
-        img4
-      </SwiperSlide>
-      <SwiperSlide>
-        {/* <img src={slide5} alt="slide5" /> */}
-        img5
+      <SwiperSlide className="swiperSlide_item">
+        <div className="item_inner">
+          <img src={slide1} alt={slide1} />
+        </div>
       </SwiperSlide>
     </Swiper>
   );
---
+++ src/components/data/mainSlideImg.js
@@ -0,0 +1,35 @@
+import slide1 from "../../assets/image/mainSlideImg1.webp";
+import slide2 from "../../assets/image/mainSlideImg2.webp";
+import slide3 from "../../assets/image/mainSlideImg3.webp";
+import slide4 from "../../assets/image/mainSlideImg4.webp";
+import slide5 from "../../assets/image/mainSlideImg5.webp";
+import slide6 from "../../assets/image/mainSlideImg6.webp";
+import slide7 from "../../assets/image/mainSlideImg7.webp";
+import slide8 from "../../assets/image/mainSlideImg8.webp";
+
+export const mainSlideImg = [
+  {
+    img: { slide1 },
+  },
+  {
+    img: { slide2 },
+  },
+  {
+    img: { slide3 },
+  },
+  {
+    img: { slide4 },
+  },
+  {
+    img: { slide5 },
+  },
+  {
+    img: { slide6 },
+  },
+  {
+    img: { slide7 },
+  },
+  {
+    img: { slide8 },
+  },
+];
