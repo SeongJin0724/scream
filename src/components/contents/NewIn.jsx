e7082e6487a95e448666f2b1524bf23018a3ca17
jimmy <skawk3235@gmail.com>
2024-04-25 17:58:50 +0900

    🚧 메인 신규판매

--- src/components/contents/NewIn.jsx
+++ src/components/contents/NewIn.jsx
@@ -12,11 +12,13 @@ export default function NewIn() {
       const response = await axios.get(
         `${process.env.REACT_APP_API_URL}/api/newin?offset=${offset}`
       );
-      console.log(response);
-      setProducts((prevProducts) => [...prevProducts, ...response.data]);
+      console.log(response.data);
+      setProducts((prevProducts) => [...prevProducts, ...response.data[0]]);
+      console.log(products);
       setOffset((prevOffset) => prevOffset + 5);
       if (offset + 5 >= 15) {
         setShowMore(false);
+        setOffset(0);
       }
     } catch (error) {
       console.log(error);
@@ -24,6 +26,7 @@ export default function NewIn() {
   };
 
   useEffect(() => {
+    setShowMore(true);
     fetchProducts();
   }, []);
 
@@ -34,16 +37,20 @@ export default function NewIn() {
         <p>신규 판매 상품</p>
       </div>
 
-      <ul>
+      <ul className="newIn_lists_wrap">
         {products.length > 0 ? (
           products.map((product) => (
-            <li key={product.itemkey}>
+            <li key={product.itemkey} className="newIn_item">
               <Link>
-                <img src={product.img} alt={product.title} />
-                <div>
-                  <p>{product.brand}</p>
-                  <p>{product.title}</p>
-                  <p>
+                <img
+                  src={product.img}
+                  alt={product.title}
+                  className="newIn_item_img"
+                />
+                <div className="newIn_item_desc">
+                  <p className="brandName">{product.brand}</p>
+                  <p className="productName">{product.title}</p>
+                  <p className="price">
                     {new Intl.NumberFormat("ko-KR").format(product.launchPrice)}
                     원
                   </p>
@@ -56,7 +63,13 @@ export default function NewIn() {
         )}
       </ul>
 
-      {showMore && <button onClick={fetchProducts}>더보기</button>}
+      {showMore && (
+        <div className="moreBtn_wrap">
+          <button onClick={fetchProducts} className="moreBtn">
+            더보기
+          </button>
+        </div>
+      )}
     </div>
   );
 }
