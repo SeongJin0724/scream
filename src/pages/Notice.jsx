5dbe0afe432b02ef536cf542e0f3114c56a3c518
dohun <jiho3332@naver.com>
2024-04-29 16:47:06 +0900

    🚧 mypage

--- src/pages/Notice.jsx
+++ src/pages/Notice.jsx
@@ -31,6 +31,16 @@ export default function Company() {
           </p>
         </div>
       </div>
+      <div data-v-5b093f8c="" className="dropdown_head">
+        <strong data-v-5b093f8c="" className="sort">
+          이벤트
+        </strong>
+        <div data-v-5b093f8c="" className="title_box">
+          <p data-v-5b093f8c="" className="title">
+            <Link to={"/notice/3"}>[안내] DRAW - 카카오페이 유로24 응모</Link>
+          </p>
+        </div>
+      </div>
     </Main>
   );
 }
---
+++ src/pages/Notice3.jsx
@@ -0,0 +1,305 @@
+import React from "react";
+import Main from "../components/section/Main";
+import { Link } from "react-router-dom";
+export default function Company() {
+  return (
+    <Main>
+      <div data-v-5b093f8c="" className="dropdown_head">
+        <strong data-v-5b093f8c="" className="sort">
+          이벤트
+        </strong>
+        <div data-v-5b093f8c="" className="title_box">
+          <p data-v-5b093f8c="" className="title">
+            [안내] DRAW - 카카오페이 유로24 응모
+          </p>
+        </div>
+      </div>
+      <div data-v-0945aee6="" class="contentn">
+        <p>안녕하세요. KREAM 입니다.</p>
+        <p>
+          4/1(월) - 4/15(월) 진행된 카카오페이 유로24 1,2차 DRAW 응모 안내
+          드립니다.
+        </p>
+        <p>&nbsp;</p>
+        <ul>
+          <li>
+            당첨 여부는 마이 페이지 &gt; 구매 내역 &gt; 구매 입찰에서 확인
+            가능합니다.
+          </li>
+          <li>
+            당첨자에 한하여 당첨 체결 알림 드리며, 미당첨자에게는 별도의 연락을
+            드리지 않습니다.
+          </li>
+          <li>
+            당첨자에게는 오늘 중으로 상품 수령 시 필요한 서류를 가입된 이메일로
+            요청하고 있습니다. 서류를 모두 전달 받은 후에 이벤트 상품이 발송되는
+            점 유의 부탁 드립니다.
+          </li>
+        </ul>
+        <p>&nbsp;</p>
+        <p class="mb-0 text-wrap">
+          <strong>-&nbsp;</strong>
+          <strong>
+            Sony Playstation 5 Blu-ray Edition &amp; Sony Playstation 5 EA
+            Sports FC24
+          </strong>
+        </p>
+        <p>
+          <strong>&nbsp;&nbsp;</strong>
+          <span>
+            <span>
+              &nbsp;소니 플레이스테이션 5 블루레이 에디션 &amp; 소니
+              플레이스테이션 5 EA스포츠 FC24
+            </span>
+            <br />
+          </span>
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>이름</td>
+              <td>가입 이메일</td>
+            </tr>
+            <tr>
+              <td>홍길동</td>
+              <td>
+                <p>qwer@naver.com</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>&nbsp;</p>
+        <p class="mb-0 text-wrap">
+          <strong>-&nbsp;</strong>
+          <strong>
+            Supreme Hooded Soccer Jersey Black - 23FW &amp; Supreme x Umbr /o
+            Soccer Ball White - 22SS
+          </strong>
+        </p>
+        <p>
+          <strong>&nbsp;&nbsp;</strong>
+          <span>
+            <span>
+              &nbsp;슈프림 후드 사커 저지 블랙 - 23FW &amp; 슈프림 x 엄브로 사커
+              볼 화이트 - 22SS
+            </span>
+            <br />
+          </span>
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>이름</td>
+              <td>가입 이메일</td>
+            </tr>
+            <tr>
+              <td>홍길동</td>
+              <td>
+                <p>qwer@naver.com</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>&nbsp;</p>
+        <p class="mb-0 text-wrap">
+          <strong>-&nbsp;</strong>
+          <strong>
+            (W) Adidas Football Long Sleeve T-Shirt Black - KR Sizing &amp;
+            Adidas Euro 24 Pro Football White Black Glow Blue
+          </strong>
+        </p>
+        <p>
+          <strong>&nbsp;&nbsp;</strong>
+          <span>
+            <span>
+              &nbsp;(W) 아디다스 풋볼 롱슬리브 티셔츠 블랙 - KR 사이즈 &amp;
+              아디다스 유로 24 프로 풋볼 화이트 블랙 글로우 블루
+            </span>
+            <br />
+          </span>
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>이름</td>
+              <td>가입 이메일</td>
+            </tr>
+            <tr>
+              <td>홍길동</td>
+              <td>
+                <p>qwer@naver.com</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>&nbsp;</p>
+        <p class="mb-0 text-wrap">
+          <strong>-&nbsp;</strong>
+          <strong>Adidas Spain Team 24 Home Jersey</strong>
+        </p>
+        <p>
+          <strong>&nbsp;&nbsp;</strong>
+          <span>
+            <span>&nbsp;아디다스 스페인 대표팀 24 홈 저지</span>
+            <br />
+          </span>
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>이름</td>
+              <td>가입 이메일</td>
+            </tr>
+            <tr>
+              <td>홍길동</td>
+              <td>
+                <p>qwer@naver.com</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>&nbsp;</p>
+        <p class="mb-0 text-wrap">
+          <strong>-&nbsp;</strong>
+          <strong>Adidas Germany Team 24 Home Jersey</strong>
+        </p>
+        <p>
+          <strong>&nbsp;&nbsp;</strong>
+          <span>
+            <span>&nbsp;아디다스 독일 대표팀 24 홈 저지</span>
+            <br />
+          </span>
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>이름</td>
+              <td>가입 이메일</td>
+            </tr>
+            <tr>
+              <td>홍길동</td>
+              <td>
+                <p>qwer@naver.com</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>&nbsp;</p>
+        <p class="mb-0 text-wrap">
+          <strong>-&nbsp;</strong>
+          <strong>Nike France Team 24 Home Jersey</strong>
+        </p>
+        <p>
+          <strong>&nbsp;&nbsp;</strong>
+          <span>
+            <span>&nbsp;나이키 프랑스 대표팀 24 홈 저지</span>
+            <br />
+          </span>
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>이름</td>
+              <td>가입 이메일</td>
+            </tr>
+            <tr>
+              <td>홍길동</td>
+              <td>
+                <p>qwer@naver.com</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>&nbsp;</p>
+        <p class="mb-0 text-wrap">
+          <strong>-&nbsp;</strong>
+          <strong>Nike Portugal Team 24 Home Jersey</strong>
+        </p>
+        <p>
+          <strong>&nbsp;&nbsp;</strong>
+          <span>
+            <span>&nbsp;나이키 포르투갈 대표팀 24 홈 저지</span>
+            <br />
+          </span>
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>이름</td>
+              <td>가입 이메일</td>
+            </tr>
+            <tr>
+              <td>홍길동</td>
+              <td>
+                <p>qwer@naver.com</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>&nbsp;</p>
+        <p class="mb-0 text-wrap">
+          <strong>-&nbsp;</strong>
+          <strong>Nike England Team 24 Home Jersey</strong>
+        </p>
+        <p>
+          <strong>&nbsp;&nbsp;</strong>
+          <span>
+            <span>&nbsp;나이키 잉글랜드 대표팀 24 홈 저지</span>
+            <br />
+          </span>
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>이름</td>
+              <td>가입 이메일</td>
+            </tr>
+            <tr>
+              <td>홍길동</td>
+              <td>qwer@naver.com</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>&nbsp;</p>
+        <p>
+          감사합니다.
+          <br />
+          <br />
+        </p>
+        <p>&nbsp;</p>
+        <p>
+          <strong>유의사항</strong>
+        </p>
+        <p>
+          *당첨 체결 시 등록한 결제정보로 결제가 진행되지 않으면, 당첨은 즉시
+          취소되며 후 순위 당첨자로 변경됩니다.
+        </p>
+        <p>
+          *제세공과금은 KREAM에서 부담하며, 제세공과금 처리를 위해 가입된 이메일
+          주소로 신분증 등 서류를 요청 드립니다.
+        </p>
+        <p>
+          *이메일로 요청한 서류(신분증/ 개인정보수집이용동의서)가 당첨자 발표
+          공지 시점으로부터 72시간 내 제출되지 않을 경우 당첨은 취소되며, 이로
+          인한 재추첨은 진행되지 않습니다.&nbsp;
+        </p>
+        <p>
+          *회원정보가 일치하지 않거나 부당한 방법으로 응모한 고객의 경우, 구매
+          취소 및 추후 이벤트 응모 시 불이익을 받을 수 있습니다.
+        </p>
+        <p>
+          *잘못된 정보를 기입하여 발생된 불이익에 대해서는 당사는 책임이 없음을
+          알려드립니다.
+        </p>
+        <p>*지급된 이벤트 상품은 교환 및 환불이 불가합니다.</p>
+        <p>
+          *이벤트 상품은 출시 연기 및 수급 상황에 따라 배송이 늦어질 수
+          있습니다.
+        </p>
+      </div>
+      <Link className="noticehome" to={"/notice"}>
+        <input type="button" id="notice_button" value="목록보기" />
+      </Link>
+    </Main>
+  );
+}
