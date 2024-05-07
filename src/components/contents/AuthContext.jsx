61b0a5d9532cb135dac8768101d17d969f1c45ca
lsj <thls702464@gmail.com>
2024-05-07 14:01:52 +0900

    🚧 상세페이지작업

--- src/components/contents/AuthContext.jsx
+++ src/components/contents/AuthContext.jsx
@@ -27,6 +27,8 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
