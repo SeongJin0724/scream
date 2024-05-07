7f6e5c5cb71fe20e3259426b2070ea2798c7fb1a
lsj <thls702464@gmail.com>
2024-05-07 14:52:02 +0900

    🚧 유저 정보 업데이트

--- src/components/contents/AuthContext.jsx
+++ src/components/contents/AuthContext.jsx
@@ -26,8 +26,34 @@ export const AuthProvider = ({ children }) => {
     console.log(token);
   }, []);
 
+  const updateUser = async (updatedUserInfo) => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/updateUser`,
+        {
+          method: "POST", // 또는 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+            // 토큰이 필요한 경우 여기에 추가
+          },
+          body: JSON.stringify(updatedUserInfo),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("유저 정보 업데이트 실패");
+      }
+      const updatedUser = await response.json();
+
+      // 로컬 스토리지와 상태 업데이트
+      localStorage.setItem("user", JSON.stringify(updatedUser));
+      setUser(updatedUser);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, updateUser }}>
       {children}
     </AuthContext.Provider>
   );
