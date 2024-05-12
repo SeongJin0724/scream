ba437b707387cabd6fa4014ab0475d3b3e9da648
dohun <jiho3332@gmail.com>
2024-05-12 14:50:34 +0900

    🚧

--- src/pages/Notice.jsx
+++ src/pages/Notice.jsx
@@ -5,7 +5,52 @@ import { Link } from "react-router-dom";
 export default function Company() {
   return (
     <Main>
+      <div className="profilechange_container">
+        <nav className="profilechange_nav">
+          <h3 className="profilechange_nav_title">마이 페이지</h3>
+          <ul className="profilechange_shopinfo">
+            <li className="profilechange_shopinfo_li">
+              <h3 className="profilechange_shopinfo_title">쇼핑 정보</h3>
+              <ul className="profilechange_shopinfo_sub_ul">
+                <li className="profilechange_shopinfo_sub_li">
+                  <a href="/company">구매내역</a>
+                </li>
+                <li className="profilechange_shopinfo_sub_li">
+                  <a href="/company">판매내역</a>
+                </li>
+                <li className="profilechange_shopinfo_sub_li">
+                  <a href="/company">관심</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul className="profilechange_myinfo">
+            <li className="profilechange_myinfo_li">
+              <h3 className="profilechange_myinfo_title">내 정보</h3>
+              <ul className="profilechange_myinfo_sub_ul">
+                <li className="profilechange_myinfo_sub_li">
+                  <a href="/mypage/infochange">로그인 정보</a>
+                </li>
+                <li className="profilechange_myinfo_sub_li">
+                  <a href="/mypage/profilechange">프로필 관리</a>
+                </li>
+                <li className="profilechange_myinfo_sub_li">
+                  <a href="/mypage/address">주소록</a>
+                </li>
+                <li className="profilechange_myinfo_sub_li">
+                  <a href="/company">결제 정보</a>
+                </li>
+                <li className="profilechange_myinfo_sub_li">
+                  <a href="/mypage/account">판매 정산 계좌</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <div className="profilechange_wrap">
+          <div className="profilechange_wrap_border">
       <h2 className="accordionh2">공지사항</h2>
+          </div>
       <div data-v-5b093f8c="" className="dropdown_head">
         <strong data-v-5b093f8c="" className="sort">
           이벤트
@@ -41,6 +86,8 @@ export default function Company() {
           </p>
         </div>
       </div>
+      </div>
+    </div>
     </Main>
   );
 }
