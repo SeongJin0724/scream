b12891c5184cb5f1730355798f447bfea0ef8787
sumin <suminc255@gmail.com>
2024-05-15 15:37:54 +0900

    🚧 마이페이지

--- src/components/contents/MyPageHome.jsx
+++ src/components/contents/MyPageHome.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from "react";
-import { SlArrowRight } from "react-icons/sl";
 import { useAuth } from "../contents/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import DealDetail from "./DealDetail";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function MyPageHome() {
   const { user, loading } = useAuth();
-
   const navigate = useNavigate();
   const token = localStorage.getItem("accessToken");
+
   useEffect(() => {
     if (!token) {
       return navigate("/login");
     }
   }, [token]);
+
   if (loading) {
     return <div>로딩중입니다.</div>;
   }
@@ -20,95 +23,39 @@ export default function MyPageHome() {
     <div className="mypage_wrap">
       <div className="mypage_profile">
         <div className="mypage_userinfo">
-          <div className="mypage_userinfo_image">
-            <img
-              data-v-ed683452=""
-              src="https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg"
-              alt="사용자 이미지"
-              className="thumb_img"
-            />
-          </div>
-          <div className="mypage_userinfo_info">
-            <div className="mypage_userinfo_info_name">{user.name}</div>
-            <div className="mypage_userinfo_info_email">{user.email}</div>
+          <img
+            data-v-ed683452=""
+            src="https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg"
+            alt="user_img"
+            className="thumb_img user_img"
+          />
+          <div className="user_detail">
+            <p className="name">{user.name}</p>
+            <p className="email">{user.email}</p>
           </div>
         </div>
-        <div className="mypage_profile_settings">
-          <div className="mypage_profile_settings_setting">
-            <a href="/company" type="button">
-              프로필 관리
-            </a>
-          </div>
-          <div className="mypage_profile_settings_mystyle">
-            <a href="/company" type="button">
-              내 스타일
-            </a>
-          </div>
+
+        <div className="setting_profile">
+          <Link to="/mypage/infochange" className="goToLink">
+            내 정보
+          </Link>
+          <Link to="" className="goToLink">
+            내 스타일
+          </Link>
         </div>
       </div>
-      <div className="mypage_buy">
-        <div className="mypage_buy_title">
-          <h3 className="mypage_buy_title_h3">구매 내역</h3>
-          <a href="/company" className="mypage_buy_title_a">
-            더보기
-            <SlArrowRight />
-          </a>
+
+      <DealDetail type="구매" user={user.user_id} />
+      <DealDetail type="판매" user={user.user_id} />
+
+      <div className="dealDetail_wrap">
+        <div className="detail_header">
+          <h3 className="header_title">위시리스트</h3>
+          <Link className="showMore_Btn">
+            더보기 <FontAwesomeIcon icon={faAngleRight} />
+          </Link>
         </div>
-        <div className="mypage_buy_purchase">
-          <div className="mypage_buy_purchase_list">
-            <div className="mypage_buy_purchase_list1">
-              전체
-              <div className="mypage_buy_purchase_list1_count"></div>
-            </div>
-            <div className="mypage_buy_purchase_list2">
-              진행중<div className="mypage_buy_purchase_list2_count"></div>
-            </div>
-            <div className="mypage_buy_purchase_list3">
-              종료<div className="mypage_buy_purchase_list3_count"></div>
-            </div>
-          </div>
-          <div className="mypage_buy_purchase_all">
-            <p>거래 내역이 없습니다.</p>
-          </div>
-        </div>
-      </div>
-      <div className="mypage_sell">
-        <div className="mypage_sell_title">
-          <h3 className="mypage_sell_title_h3">판매 내역</h3>
-          <a href="/company" className="mypage_sell_title_a">
-            더보기
-            <SlArrowRight />
-          </a>
-        </div>
-        <div className="mypage_sell_sale">
-          <div className="mypage_sell_sale_list">
-            <div className="mypage_sell_sale_list1">
-              전체
-              <div className="mypage_sell_sale_list1_count"></div>
-            </div>
-            <div className="mypage_sell_sale_list2">
-              진행중<div className="mypage_sell_sale_list2_count"></div>
-            </div>
-            <div className="mypage_sell_sale_list3">
-              종료<div className="mypage_sell_sale_list3_count"></div>
-            </div>
-          </div>
-          <div className="mypage_sell_sale_all">
-            <p>거래 내역이 없습니다.</p>
-          </div>
-        </div>
-      </div>
-      <div className="mypage_wish">
-        <div className="mypage_wish_title">
-          <h3 className="mypage_wish_title_h3">위시리스트</h3>
-          <a href="/company" className="mypage_wish_title_a">
-            더보기
-            <SlArrowRight />
-          </a>
-        </div>
-        <div className="mypage_wish_all">
-          <p>추가하신 관심 상품이 없습니다.</p>
-        </div>
+        <div className="wish_list">추가하신 관심 상품이 없습니다.</div>
       </div>
     </div>
   );
